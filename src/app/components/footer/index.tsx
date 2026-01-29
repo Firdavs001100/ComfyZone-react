3933abdc65d70f2b9b880d9c3805fff86401a08f
Firdavs001100 <foruseofbusiness@gmail.com>
2026-01-29 11:21:06 +0900

    feat: add some materials of the project

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Container, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Footers = styled.div`
+  width: 100%;
+  height: 590px;
+  display: flex;
+  background: #343434;
+  background-size: cover;
+`;
+
+export default function Footer() {
+  const authMember = null;
+
+  return (
+    <Footers>
+      <Container></Container>
+    </Footers>
+  );
+}
