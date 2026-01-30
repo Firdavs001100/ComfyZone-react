110edc7335a7dbe718fc6cd89aa17ed1f4454907
Firdavs001100 <foruseofbusiness@gmail.com>
2026-01-30 19:46:32 +0900

    fix: develop userPage css

--- src/app/screens/userPage/index.tsx
+++ src/app/screens/userPage/index.tsx
@@ -20,7 +20,7 @@ export default function UserPage() {
         <Stack className={"my-page-frame"}>
           <Stack className={"my-page-left"}>
             <Box display={"flex"} flexDirection={"column"}>
-              <Box className={"menu-name"}>Modify Member Details</Box>
+              <Box className={"menu-name"}>Member Details</Box>
               <Box className={"menu-content"}>
                 <Settings />
               </Box>
